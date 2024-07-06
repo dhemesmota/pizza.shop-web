f06090fcf07b1929a4bb27f90303fdd0b5881ae4
Dhemes Mota <dhemes.mota@gmail.com>
2024-07-06 14:46:43 -0300

    feat: add test nav link

--- src/components/nav-link.tsx
+++ src/components/nav-link.tsx
@@ -7,8 +7,8 @@ export function NavLink(props: NavLinkProps) {
 
   return (
     <Link
-      data-active={pathname === props.to}
-      className="flex items-center gap-1.5 text-sm font-medium text-muted-foreground hover:text-foreground data-[active=true]:text-foreground"
+      data-current={pathname === props.to}
+      className="flex items-center gap-1.5 text-sm font-medium text-muted-foreground hover:text-foreground data-[current=true]:text-foreground"
       {...props}
     />
   )
